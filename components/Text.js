5533aa506b2a16e6a81009d5a96afa3826542428
Marco Zampetti <m.zampetti@cantierecreativo.net>
2022-01-26 19:54:37 +0100

    Remove alt color of text block

--- components/Text.js
+++ components/Text.js
@@ -2,7 +2,7 @@ export default function Text({ block }) {
   return (
     <section className="container py-12 lg:py-24 xl:py-36">
       <h2 className="font-heading font-medium text-accent uppercase text-xl mb-6 xl:text-2xl xl:mb-12">{block.title}</h2>
-      <p className="font-heading text-alt max-w-xl xl:text-lg">{block.text}</p>
+      <p className="font-heading max-w-xl xl:text-lg">{block.text}</p>
     </section>
   );
 }
