0177462e0940aa3533ebfd3b61cf33490ad2f2c0
Marco Zampetti <m.zampetti@cantierecreativo.net>
2022-01-26 12:13:21 +0100

    Setup tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,14 +14,34 @@ module.exports = {
   ],
   theme: {
     colors: {
-      primary: withOpacityValue('--color-primary'),
-      secondary: '#ecc94b',
+      primary: withOpacityValue('--color-text'),
+      alt: withOpacityValue('--color-text-alt'),
+      accent: withOpacityValue('--color-accent'),
+      back: withOpacityValue('--color-back'),
+      'back-alt': withOpacityValue('--color-back-alt')
+    },
+    container: {
+      center: true,
+      padding: '2rem'
     },
     fontFamily: {
       'heading': ["var(--font-heading)", 'sans-serif'],
       'body': ["var(--font-body)", 'serif']
     },
-    extend: {},
+    fontSize: {
+      xs: ['14px', '24px'],
+      sm: ['16px', '20px'],
+      base: ['18px', '28px'],
+      lg: ['22px', '35px'],
+      xl: ['35px', '45px'],
+      '2xl': ['50px', '60px'],
+      '3xl': ['70px', '70px']
+    },
+    extend: {
+      letterSpacing: {
+        widest: '.25em'
+      },
+    },
   },
   plugins: [],
 }
