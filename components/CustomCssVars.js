f7c02f5ac3fff57e203704b8c6e5b6e083d79f7c
Marco Zampetti <m.zampetti@cantierecreativo.net>
2022-02-01 12:52:14 +0100

    Custom swiper navigation

--- components/CustomCssVars.js
+++ components/CustomCssVars.js
@@ -2,6 +2,15 @@ function rgbColor(color) {
   return `${color.red} ${color.green} ${color.blue}`
 }
 
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(color) {
+  return "#" + componentToHex(color.red) + componentToHex(color.green) + componentToHex(color.blue);
+}
+
 export default function CustomCssVars({ data }) {
   const { colorText,
     colorTextAlt,
@@ -15,6 +24,7 @@ export default function CustomCssVars({ data }) {
   return (
     <style global jsx>{`
       :root {
+        --color-white: 255 255 255;
         --color-text: ${rgbColor(colorText)};
         --color-text-alt: ${rgbColor(colorTextAlt)};
         --color-back: ${rgbColor(colorBack)};
@@ -22,6 +32,12 @@ export default function CustomCssVars({ data }) {
         --color-accent: ${rgbColor(colorAccent)};
         --font-body: ${fontBody};
         --font-heading: ${fontHeading};
+        --swiper-theme-color: ${rgbToHex(colorAccent)};
+        --swiper-pagination-bullet-inactive-color: #ffffff;
+        --swiper-pagination-bullet-inactive-opacity: 1;
+        --swiper-navigation-size: 2.5rem;
+        --swiper-pagination-bullet-horizontal-gap: 1rem;
+        --swiper-margin: 0.8rem;
       }
     `}</style>
   );
