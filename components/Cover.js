fe365c1adf0710f208057bb850f845bff6c58022
Marco Zampetti <m.zampetti@cantierecreativo.net>
2022-02-01 10:50:40 +0100

    Add swiper navigation and pagination modules to cover blocks

--- components/Cover.js
+++ components/Cover.js
@@ -1,11 +1,17 @@
 import { Image } from "react-datocms";
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, A11y } from 'swiper';
 
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 export default function Cover({ block }) {
   return (
     <Swiper
+      modules={[Navigation, Pagination, A11y]}
+      navigation
+      pagination={{ clickable: true }}
       slidesPerView={1}
     >
       {block.images && Object.values(block.images).map((image) => (
