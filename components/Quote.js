f6b6429f39fba3d0eab0df72f23fb2bce5c04edc
Marco Zampetti <m.zampetti@cantierecreativo.net>
2022-02-03 18:10:13 +0100

    Basic quote style

--- components/Quote.js
+++ components/Quote.js
@@ -1,10 +1,10 @@
 export default function Quote({ block }) {
   return (
-    <section className="container py-12 lg:py-24 xl:py-36">
-      <div className="font-heading font-medium text-accent uppercase text-xl mb-6 xl:text-2xl xl:mb-12">
+    <section className="container text-center py-12 lg:py-24 xl:py-36">
+      <div className="font-heading text-alt mb-6 lg:text-xl xl:mb-12">
         {block.text}
       </div>
-      <div className="font-heading max-w-xl xl:text-lg">
+      <div className="font-bold text-xs uppercase text-accent tracking-widest my-2">
         {block.authorName}
       </div>
     </section>
