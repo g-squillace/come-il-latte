042f6c790df8a4f94a97d02307f4ae572899c3b5
Marco Zampetti <m.zampetti@cantierecreativo.net>
2022-02-07 11:35:18 +0100

    Style quote component

--- components/Quote.js
+++ components/Quote.js
@@ -1,7 +1,7 @@
 export default function Quote({ block }) {
   return (
-    <section className="container text-center py-12 lg:py-24 xl:py-36">
-      <div className="font-heading text-alt mb-6 lg:text-lg xl:mb-12">
+    <section className="container text-center relative mt-12 py-20 lg:mt-24 lg:py-36 before:content-['\201c'] before:text-accent before:absolute before:text-[6rem] before:leading-none before:left-1/2 before:-translate-x-1/2 before:-top-0 lg:before:text-[10rem]">
+      <div className="font-heading italic text-alt max-w-prose mx-auto mb-6 lg:text-lg xl:mb-12">
         {block.text}
       </div>
       <div className="font-bold text-xs uppercase text-accent tracking-widest my-2">
